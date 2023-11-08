32f955bb92499dff16044e3e1693f3d3a9b0e568
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-11-07 19:41:43 -0800

    Lab 4 - Tests

--- MMABooksTests/CustomerTests.cs
+++ MMABooksTests/CustomerTests.cs
@@ -1,92 +1,94 @@
-using System.Collections.Generic;
-using System.Linq;
-using System;
-
 using NUnit.Framework;
-using MMABooksEFClasses.MarisModels;
+//using MMABooksEFClasses.MarisModels;
+using MMABooksEFClasses.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace MMABooksTests
-{
+namespace MMABooksTests {
     [TestFixture]
-    public class CustomerTests
-    {
-        /*
+    public class CustomerTests {
         MMABooksContext dbContext;
         Customer? c;
         List<Customer>? customers;
 
         [SetUp]
-        public void Setup()
-        {
+        public void Setup() {
             dbContext = new MMABooksContext();
             dbContext.Database.ExecuteSqlRaw("call usp_testingResetData()");
         }
 
         [Test]
-        public void GetAllTest()
-        {
-        }
-
-        [Test]
-        public void GetByPrimaryKeyTest()
-        {
-        }
-
-        [Test]
-        public void GetUsingWhere()
-        {
-            // get a list of all of the customers who live in OR
-        }
-
-        [Test]
-        public void GetWithInvoicesTest()
-        {
-           // get the customer whose id is 20 and all of the invoices for that customer
-        }
-
-        [Test]
-        public void GetWithJoinTest()
-        {
-            // get a list of objects that include the customer id, name, statecode and statename
-            var customers = dbContext.Customers.Join(
-               dbContext.States,
-               c => c.StateCode,
-               s => s.StateCode,
-               (c, s) => new { c.CustomerId, c.Name, c.StateCode, s.StateName }).OrderBy(r => r.StateName).ToList();
+        public void GetAllTest() {
+            customers = dbContext.Customers.OrderBy(c => c.Name).ToList();
             Assert.AreEqual(696, customers.Count);
-            // I wouldn't normally print here but this lets you see what each object looks like
-            foreach (var c in customers)
-            {
-                Console.WriteLine(c);
-            }
+            Assert.AreEqual("Abeyatunge, Derek", customers[0].Name);
+            PrintAll(customers);
         }
 
         [Test]
-        public void DeleteTest()
-        {
-
+        public void GetByPrimaryKeyTest() {
+            c = dbContext.Customers.Find(121);
+            Assert.IsNotNull(c);
+            Assert.AreEqual("Demith, Bob", c?.Name);
+            Console.WriteLine(c);
         }
 
         [Test]
-        public void CreateTest()
-        {
-
+        public void GetUsingWhere() {
+            customers = dbContext.Customers.Where(c => c.State.Equals("OR")).OrderBy(c => c.Name).ToList();
+            Assert.AreEqual(5, customers.Count);
+            Assert.AreEqual("Erpenbach, Lee", customers[0].Name);
+            PrintAll(customers);
         }
 
         [Test]
-        public void UpdateTest()
-        {
-
+        public void GetWithInvoicesTest() {
+            c = dbContext.Customers.Include("Invoices").Where(c => c.CustomerId == 20).SingleOrDefault();
+            Assert.IsNotNull(c);
+            Assert.AreEqual("Chamberland, Sarah", c?.Name);
+            Assert.AreEqual(3, c.Invoices.Count);
+            Console.WriteLine(c);
         }
 
-        public void PrintAll(List<Customer> customers)
-        {
-            foreach (Customer c in customers)
-            {
-                Console.WriteLine(c);
-            }
+        [Test]
+        public void GetWithJoinTest() {
+            var customers = dbContext.Customers.Join(dbContext.States, c => c.State, s => s.StateCode, (c, s) => new { c.CustomerId, c.Name, c.State, s.StateName }).OrderBy(r => r.StateName).ToList();
+            Assert.AreEqual(696, customers.Count);
+            foreach (var c in customers) Console.WriteLine(c);
         }
-        */
+
+        [Test]
+        public void DeleteTest() {
+            c = dbContext.Customers.Find(1);
+            dbContext.Customers.Remove(c);
+            dbContext.SaveChanges();
+            Assert.IsNull(dbContext.Customers.Find(1));
+        }
+
+        [Test]
+        public void CreateTest() {
+            c = new Customer {
+                CustomerId = 1000,
+                Name = "1",
+                Address = "2",
+                City = "3",
+                State = "FL",
+                ZipCode = "4"
+            };
+            dbContext.Customers.Add(c);
+            dbContext.SaveChanges();
+            Assert.IsNotNull(dbContext.Customers.Find(1000));
+        }
+
+        [Test]
+        public void UpdateTest() {
+            c = dbContext.Customers.Find(200);
+            c.Name = "Name was changed";
+            dbContext.Customers.Update(c);
+            dbContext.SaveChanges();
+            c = dbContext.Customers.Find(200);
+            Assert.AreEqual("Name was changed", c?.Name);
+        }
+
+        public static void PrintAll(List<Customer> customers) { foreach (Customer c in customers) Console.WriteLine(c); }
     }
 }
--- MMABooksTests/StateTests.cs
+++ MMABooksTests/StateTests.cs
@@ -1,50 +1,39 @@
-using System.Collections.Generic;
-using System.Linq;
-using System;
-
 using NUnit.Framework;
-using MMABooksEFClasses.MarisModels;
+//using MMABooksEFClasses.MarisModels;
+using MMABooksEFClasses.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace MMABooksTests
-{
+namespace MMABooksTests {
     [TestFixture]
-    public class StateTests
-    {
-        // ignore this warning about making dbContext nullable.
-        // if you add the ?, you'll get a warning wherever you use dbContext
+    public class StateTests {
         MMABooksContext dbContext;
         State? s;
         List<State>? states;
 
         [SetUp]
-        public void Setup()
-        {
+        public void Setup() {
             dbContext = new MMABooksContext();
             dbContext.Database.ExecuteSqlRaw("call usp_testingResetData()");
         }
 
         [Test]
-        public void GetAllTest()
-        {
+        public void GetAllTest() {
             states = dbContext.States.OrderBy(s => s.StateName).ToList();
-            Assert.AreEqual(52, states.Count);
+            Assert.AreEqual(53, states.Count);
             Assert.AreEqual("Alabama", states[0].StateName);
             PrintAll(states);
         }
 
         [Test]
-        public void GetByPrimaryKeyTest()
-        {
+        public void GetByPrimaryKeyTest() {
             s = dbContext.States.Find("OR");
             Assert.IsNotNull(s);
-            Assert.AreEqual("Ore", s.StateName);
+            Assert.AreEqual("Ore", s?.StateName);
             Console.WriteLine(s);
         }
 
         [Test]
-        public void GetUsingWhere()
-        {
+        public void GetUsingWhere() {
             states = dbContext.States.Where(s => s.StateName.StartsWith("A")).OrderBy(s => s.StateName).ToList();
             Assert.AreEqual(4, states.Count);
             Assert.AreEqual("Alabama", states[0].StateName);
@@ -52,42 +41,43 @@ namespace MMABooksTests
         }
 
         [Test]
-        public void GetWithCustomersTest()
-        {
+        public void GetWithCustomersTest() {
             s = dbContext.States.Include("Customers").Where(s => s.StateCode == "OR").SingleOrDefault();
             Assert.IsNotNull(s);
-            Assert.AreEqual("Ore", s.StateName);
+            Assert.AreEqual("Ore", s?.StateName);
             Assert.AreEqual(5, s.Customers.Count);
             Console.WriteLine(s);
         }
 
         [Test]
-        public void DeleteTest()
-        {
-            s = dbContext.States.Find("HI");
+        public void DeleteTest() {
+            s = dbContext.States.Find("ZZ");
             dbContext.States.Remove(s);
             dbContext.SaveChanges();
-            Assert.IsNull(dbContext.States.Find("HI"));
+            Assert.IsNull(dbContext.States.Find("ZZ"));
         }
 
         [Test]
-        public void CreateTest()
-        {
-
+        public void CreateTest() {
+            s = new State {
+                StateCode = "ZZ",
+                StateName = "ZooZoo"
+            };
+            dbContext.States.Add(s);
+            dbContext.SaveChanges();
+            Assert.IsNotNull(dbContext.States.Find("ZZ"));
         }
 
         [Test]
-        public void UpdateTest()
-        {
-
+        public void UpdateTest() {
+            s = dbContext.States.Find("OR");
+            s.StateName = "Ore";
+            dbContext.States.Update(s);
+            dbContext.SaveChanges();
+            s = dbContext.States.Find("OR");
+            Assert.AreEqual("Ore", s?.StateName);
         }
 
-        public void PrintAll(List<State> states)
-        {
-            foreach (State s in states)
-            {
-                Console.WriteLine(s);
-            }
-        }
+        public static void PrintAll(List<State> states) { foreach (State s in states) Console.WriteLine(s); }
     }
 }
